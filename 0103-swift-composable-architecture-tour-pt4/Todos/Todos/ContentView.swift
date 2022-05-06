baf5a3808d6ddda44fcbb347aa9fa36379e95363
Tyler <haikusw@gmail.com>
2022-05-06 16:33:42 -0700

    Convert to modern Composable architecture and remove use of indexes.  Turns out this was mostly done in the Examples version in Composable Architecture.  But I learned a lot doing the conversion :)

--- 0103-swift-composable-architecture-tour-pt4/Todos/Todos/ContentView.swift
+++ 0103-swift-composable-architecture-tour-pt4/Todos/Todos/ContentView.swift
@@ -28,12 +28,12 @@ let todoReducer = Reducer<Todo, TodoAction, TodoEnvironment> { state, action, en
 }
 
 struct AppState: Equatable {
-  var todos: [Todo] = []
+  var todos: IdentifiedArrayOf<Todo> = []
 }
 
 enum AppAction: Equatable {
   case addButtonTapped
-  case todo(index: Int, action: TodoAction)
+  case todo(id: UUID, action: TodoAction)
   case todoDelayCompleted
 //  case todoCheckboxTapped(index: Int)
 //  case todoTextFieldChanged(index: Int, text: String)
@@ -59,7 +59,7 @@ struct AppEnvironment {
 let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
   todoReducer.forEach(
     state: \AppState.todos,
-    action: /AppAction.todo(index:action:),
+    action: /AppAction.todo(id:action:),
     environment: { _ in TodoEnvironment() }
   ),
   Reducer { state, action, environment in
@@ -68,13 +68,13 @@ let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
       state.todos.insert(Todo(id: environment.uuid()), at: 0)
       return .none
       
-    case .todo(index: _, action: .checkboxTapped):
+    case .todo(id: _, action: .checkboxTapped):
       struct CancelDelayId: Hashable {}
 
       return Effect(value: AppAction.todoDelayCompleted)
         .debounce(id: CancelDelayId(), for: 1, scheduler: environment.mainQueue)
       
-    case .todo(index: let index, action: let action):
+    case .todo(id: let id, action: let action):
       return .none
       
     case .todoDelayCompleted:
@@ -86,6 +86,7 @@ let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
             || lhs.offset < rhs.offset
       }
       .map(\.element)
+      .reduce(into: IdentifiedArray<UUID,Todo>()) { $0.append($1) }
       
       return .none
     }
@@ -114,7 +115,7 @@ struct ContentView: View {
       WithViewStore(self.store) { viewStore in
         List {
           ForEachStore(
-            self.store.scope(state: \.todos, action: AppAction.todo(index:action:)),
+            self.store.scope(state: \.todos, action: AppAction.todo(id:action:)),
             content: TodoView.init(store:)
           )
         }
--- 0103-swift-composable-architecture-tour-pt4/Todos/TodosTests/TodosTests.swift
+++ 0103-swift-composable-architecture-tour-pt4/Todos/TodosTests/TodosTests.swift
@@ -3,38 +3,32 @@ import XCTest
 @testable import Todos
 
 class TodosTests: XCTestCase {
-  let scheduler = DispatchQueue.testScheduler
+  let scheduler = DispatchQueue.test
 
   func testCompletingTodo() {
+    let todos: IdentifiedArrayOf<Todo> = [
+      Todo(
+        description: "Milk",
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+        isComplete: false
+      )
+    ]
+    
     let store = TestStore(
-      initialState: AppState(
-        todos: [
-          Todo(
-            description: "Milk",
-            id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
-            isComplete: false
-          )
-        ]
-      ),
+      initialState: AppState(todos: todos),
       reducer: appReducer,
       environment: AppEnvironment(
         mainQueue: scheduler.eraseToAnyScheduler(),
         uuid: { fatalError("unimplemented") }
       )
     )
-
-//    scheduler.advance(by: 1000)
     
-    store.assert(
-      .send(.todo(index: 0, action: .checkboxTapped)) {
-        $0.todos[0].isComplete = true
-      },
-      .do {
-//        _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 1)
-        self.scheduler.advance(by: 1)
-      },
-      .receive(.todoDelayCompleted)
-    )
+    store.send(.todo(id: todos[0].id, action: .checkboxTapped)) {
+      $0.todos[id: $0.todos[0].id]?.isComplete = true
+    }
+    
+    self.scheduler.advance(by: 1)
+    store.receive(.todoDelayCompleted)
   }
   
   func testAddTodo() {
@@ -47,8 +41,7 @@ class TodosTests: XCTestCase {
       )
     )
     
-    store.assert(
-      .send(.addButtonTapped) {
+    store.send(.addButtonTapped) {
         $0.todos = [
           Todo(
             description: "",
@@ -57,25 +50,24 @@ class TodosTests: XCTestCase {
           )
         ]
       }
-    )
   }
   
   func testTodoSorting() {
-    let store = TestStore(
-      initialState: AppState(
-        todos: [
-          Todo(
-            description: "Milk",
-            id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
-            isComplete: false
-          ),
-          Todo(
-            description: "Eggs",
-            id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
-            isComplete: false
-          )
-        ]
+    let todos: IdentifiedArray<UUID,Todo> = [
+      Todo(
+        description: "Milk",
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+        isComplete: false
       ),
+      Todo(
+        description: "Eggs",
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+        isComplete: false
+      )
+    ]
+
+    let store = TestStore(
+      initialState: AppState(todos: todos),
       reducer: appReducer,
       environment: AppEnvironment(
       mainQueue: scheduler.eraseToAnyScheduler(),
@@ -83,36 +75,32 @@ class TodosTests: XCTestCase {
       )
     )
     
-    store.assert(
-      .send(.todo(index: 0, action: .checkboxTapped)) {
-        $0.todos[0].isComplete = true
-      },
-      .do {
-//        _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 1)
-        self.scheduler.advance(by: 1)
-      },
-      .receive(.todoDelayCompleted) {
+    store.send(.todo(id: todos[0].id, action: .checkboxTapped)) {
+      $0.todos[id: todos[0].id]?.isComplete = true
+    }
+
+    self.scheduler.advance(by: 1)
+    store.receive(.todoDelayCompleted) {
         $0.todos.swapAt(0, 1)
-      }
-    )
+    }
   }
-  
+
   func testTodoSorting_Cancellation() {
-    let store = TestStore(
-      initialState: AppState(
-        todos: [
-          Todo(
-            description: "Milk",
-            id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
-            isComplete: false
-          ),
-          Todo(
-            description: "Eggs",
-            id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
-            isComplete: false
-          )
-        ]
+    let todos: IdentifiedArray<UUID,Todo> = [
+      Todo(
+        description: "Milk",
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+        isComplete: false
       ),
+      Todo(
+        description: "Eggs",
+        id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+        isComplete: false
+      )
+    ]
+    
+    let store = TestStore(
+      initialState: AppState(todos: todos),
       reducer: appReducer,
       environment: AppEnvironment(
         mainQueue: scheduler.eraseToAnyScheduler(),
@@ -120,22 +108,17 @@ class TodosTests: XCTestCase {
       )
     )
     
-    store.assert(
-      .send(.todo(index: 0, action: .checkboxTapped)) {
-        $0.todos[0].isComplete = true
-      },
-      .do {
-//        _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 0.5)
-        self.scheduler.advance(by: 0.5)
-      },
-      .send(.todo(index: 0, action: .checkboxTapped)) {
-        $0.todos[0].isComplete = false
-      },
-      .do {
-//        _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 1)
-        self.scheduler.advance(by: 1)
-      },
-      .receive(.todoDelayCompleted)
-    )
+    store.send(.todo(id: todos[0].id, action: .checkboxTapped)) {
+      $0.todos[id: todos[0].id]?.isComplete = true
+    }
+    
+    self.scheduler.advance(by: 0.5)
+    
+    store.send(.todo(id: todos[0].id, action: .checkboxTapped)) {
+      $0.todos[id: todos[0].id]?.isComplete = false
+    }
+    self.scheduler.advance(by: 1)
+    
+    store.receive(.todoDelayCompleted)
   }
 }
